45e7de9c79fd7a3b408b81fb7c9731df894167fc
anurudhwhitehat <anurudhwhitehat@gmail.com>
2019-11-05 13:53:45 +0530

    testing about pull request

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express=require('express');
-const app=express();
-var router=express.Router();
+const express = require('express');
+const app = express();
+var router = express.Router();
 var swaggerUi = require('swagger-ui-express');
 var swaggerDocument = require('./swagger.json')
-var bodyParser=require('body-parser');
-var cors=require('cors');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}
 app.use(cors(corsOptions))
 app.use(bodyParser.json())
 
@@ -17,23 +17,23 @@ app.use(bodyParser.json())
 var models = require('./models');
 
 models.sequelize.sync().then(() => {
-    console.log('Nice,Database looks fine')
+  console.log('Nice,Database looks fine')
 }).catch(err => {
-    console.log(err, 'Something went wrong with database update !')
+  console.log(err, 'Something went wrong with database update !')
 })
 
 
 
-require('./routes/AuthRoutes')(app,router);
-require('./routes/DeviceRoutes')(app,router);
+require('./routes/AuthRoutes')(app, router);
+require('./routes/DeviceRoutes')(app, router);
 
 var options = {
-    customCss: '.swagger-ui .topbar { display: none }'
-  };
-  
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument,options));
-app.listen(process.env.PORT || 3000,()=>{
-    console.log('Server started !')
+  customCss: '.swagger-ui .topbar { display: none }'
+};
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server started !')
 });
 
 
