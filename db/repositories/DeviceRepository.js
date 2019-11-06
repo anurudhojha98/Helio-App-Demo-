84c5e677af951d35f6ef0c8f22a117d2f26fd517
anurudhojha98 <anurudhojha98@gmail.com>
2019-11-06 23:01:42 +0530

    add repository changes

---
+++ db/repositories/DeviceRepository.js
@@ -0,0 +1,32 @@
+
+var create_device=()=>{
+
+    //write the query to fetch data from db
+}
+var get_device=()=>{
+    //write the query to fetch data from db
+}
+
+var update_device=()=>{
+    //write the query to access data from db
+}
+var delete_device=()=>{
+    //write the query to access data from db
+}
+
+var get_device_byId=()=>{
+//write the query to access data from db
+}
+
+var list_all_devices=()=>{
+    //write the query to access data from db
+}
+
+module.exports={
+    create_device,
+    get_device,
+    get_device_byId,
+    list_all_devices,
+    delete_device,
+    update_device
+}
--- db/repositories/UserRepository.js
+++ db/repositories/UserRepository.js
@@ -1,6 +1,17 @@
+var user_signup=()=>{
+
+    //write the query to fetch data from db
+}
+var user_login=()=>{
+    //write the query to fetch data from db
+}
+
+var reset_password=()=>{
+    //write the query to access data from db
+}
 
 module.exports={
-
-
-    
+    user_login,
+    user_signup,
+    reset_password
 }
